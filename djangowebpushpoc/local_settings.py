517b5e6482dde92894fc4a57d0fe72a260e8b882
elishowk <elias@showk.me>
2019-10-22 18:23:50 +0200

    Update local_settings.py

--- djangowebpushpoc/local_settings.py
+++ djangowebpushpoc/local_settings.py
@@ -1,1 +1,3 @@
-#PUSH_NOTIFICATIONS_SETTINGS['APP_SERVER_KEY'] = 'Key from command vapid --applicationServerKey'
+#PUSH_NOTIFICATIONS_SETTINGS['APP_SERVER_KEY'] = 'Output from command vapid --applicationServerKey'
+#ALLOWED_HOSTS = ['my-dns.fr']
+#PUSH_NOTIFICATIONS_SETTINGS["WP_PRIVATE_KEY"]= "/home/myuser/private_key_generated_by_vapid.pem"
